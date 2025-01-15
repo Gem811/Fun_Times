94cf3011ecf74f06f90241039f8561bc79628775
Gem811 <166326098+Gem811@users.noreply.github.com>
2025-01-14 18:30:15 -0800

    Added the first file

---
+++ Lab_1_a/Lab_1_a/Lab_work_b_past.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+
+/*
+* int main(void) {
+	printf("Hello World!");
+}
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS // necessary to ignore scanf_s () warnings
+#include <stdio.h> // necessary to use printf () and scanf ()
+int main(void) // the starting point for all C programs
+{
+
+	/*
+	int I = 0, R = 0;
+	printf("Enter two values for Ohm's Law (I & R): ");
+	scanf_s("%d%d", &I, &R);
+	printf("%d = Voltage",I * R);
+	*/
+
+
+
+
+	return 0; // return a success status (value 0) indicating the program worked fine
+} // end of the main () function
